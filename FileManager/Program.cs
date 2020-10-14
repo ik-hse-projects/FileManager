b596cb94cec8b0a322d5f59fd4b6b8534d288ed7
Ilia Konnov <iliakonnov@gmail.com>
2020-10-14 20:51:04 +0300

    Initial commit

---
+++ FileManager/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Thuja;
+using Thuja.Widgets;
+
+namespace FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MainLoop(
+                new Label("Hello world!")
+            ).Start();
+        }
+    }
+}
